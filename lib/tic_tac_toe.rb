1e6ddc088e3c8b0da0c4eac45723dd125dee043b
Gabe Aguilar <gabe.p.aguilar@gmail.com>
2020-10-28 13:37:43 -0500

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -23,7 +23,116 @@ class TicTacToe
         puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
     end
 
-def 
+def input_to_index(user_input)
+    user_input.to_i - 1
+end 
 
+def move(position, char)
+    @board[position] = char
+  end
 
+def position_taken?(index_i)
+    if ((@board[index_i] == "X") || (@board[index_i] == "O"))
+        return true
+    else 
+        false
+    end 
+end 
+
+def valid_move?(index)
+    if index.between?(0,8) && !position_taken?(index)
+        return true 
+    else 
+        false
+    end 
+end 
+
+def turn
+    puts "Please enter 1-9:"
+    input = gets.strip
+    index = input_to_index(input)
+    char = current_player
+    if valid_move?(index)
+      move(index, char)
+      display_board
+    else
+      turn
+    end
 end
+
+
+    def turn_count
+        counter = 0
+        @board.each do |i|
+            if i == "X" || i == "O"
+                counter += 1
+            end 
+        end 
+        return counter
+    end 
+
+    def current_player
+        turn_count% 2 == 0 ? "X" : "O"
+    end 
+
+    def check_win_combo?(player, win_combo)
+        win_combo.all? do |position|
+            @board[position] == player
+        end
+    end 
+
+    def won?
+        WIN_COMBINATIONS.detect do |win_combo|
+          if (@board[win_combo[0]]) == "X" && (@board[win_combo[1]]) == "X" && (@board[win_combo[2]]) == "X"
+            return win_combo
+          elsif (@board[win_combo[0]]) == "O" && (@board[win_combo[1]]) == "O" && (@board[win_combo[2]]) == "O"
+            return win_combo
+          end
+            false
+        end
+      end
+
+    def full?
+        @board.all?{|token| token == "X" || token == "O"}
+    end
+
+    def draw?
+        !(won?) && (full?)
+      end
+
+    def over?
+        if won? || draw? || full?
+            return true
+        else 
+            return false
+        end
+    end
+
+    def winner
+        WIN_COMBINATIONS.detect do |win_combo|
+          if (@board[win_combo[0]]) == "X" && (@board[win_combo[1]]) == "X" && (@board[win_combo[2]]) == "X"
+            return "X"
+          elsif (@board[win_combo[0]]) == "O" && (@board[win_combo[1]]) == "O" && (@board[win_combo[2]]) == "O"
+            return "O"
+          else
+            nil
+          end
+        end
+      end
+    
+
+      def play
+        while over? == false
+          turn
+        end
+        if won?
+          puts "Congratulations #{winner}!"
+        elsif draw?
+          puts "Cat's Game!"
+        end
+      end
+         
+end
+
+
+
